a3147f84d66cc09eb863443cd89512b235023f33
rubanofficial <rubans082005@gmail.com>
2025-10-24 08:24:27 +0530

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
       <Footer />
