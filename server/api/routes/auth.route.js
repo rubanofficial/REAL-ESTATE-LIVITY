b40698209bb877f4d738a8df0f813894ee32386c
rubanofficial <rubans082005@gmail.com>
2025-10-30 15:10:58 +0530

    Add signin route to auth router

--- server/api/routes/auth.route.js
+++ server/api/routes/auth.route.js
@@ -4,6 +4,7 @@ import express from 'express';
 // 2. Import the controller function (the "chef")
 // We have not created this file yet, but we will next
 import { signup } from '../controllers/auth.controller.js';
+import { signin } from '../controllers/auth.controller.js';
 
 // 3. Create a new router
 const router = express.Router();
@@ -11,7 +12,7 @@ const router = express.Router();
 // 4. Define the signup route
 // When a POST request hits '/signup', call the 'signup' function
 router.post('/signup', signup);
-
+router.post('/signin', signin);
 // 5. Export the router for the main app to use
 export default router;
 
